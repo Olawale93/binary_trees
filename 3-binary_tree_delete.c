94d52631e75c6a21495f1e0a0252b3c3dab4f489
EmEch01 <rhamzee13@gmail.com>
2024-06-06 00:17:41 -0700

    3-del

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,18 +3,25 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_delete - Deletes an entire binary tree
- * @tree: Pointer to the root node of the tree to delete
- *
- * Return: void
- */
+* binary_tree_delete - Deletes an entire binary tree
+* @tree: Pointer to the root node of the tree to delete
+*
+* Return: void
+*/
 
 void binary_tree_delete(binary_tree_t *tree)
 {
 if (tree == NULL)
+{
 return;
-
+}
+else
+{
+if (tree != NULL)
+{
 binary_tree_delete(tree->left);
 binary_tree_delete(tree->right);
+}
 free(tree);
+}
 }
--- binary_trees.h
+++ binary_trees.h
@@ -27,11 +27,8 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-<<<<<<< HEAD
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-=======
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t* tree);
 
->>>>>>> c572f897d778a967717e7f30a0d1ee0a77678194
 #endif
