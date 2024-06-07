639e0d00be5e3dd9efcf63053d153903b0560955
EmEch01 <rhamzee13@gmail.com>
2024-06-07 15:53:35 -0700

    task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,33 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_ancestor - Function that checks for an ancestor
- * @first: Pointer to the first node
- * @second: Pointer to the second node
- *
- * Return: Node of the ancestor
- */
+* binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+* @first: Pointer to the first node
+* @second: Pointer to the second node
+*
+* Return: Pointer to the lowest common ancestor node of the two given nodes
+*         If no common ancestor was found, return NULL
+*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	binary_tree_t *p, *q;
+const binary_tree_t *e, *a;
 
-	if (first == NULL  || second == NULL)
-	{
-		return (NULL);
-	}
-	if (first == second)
-	{
-		return ((binary_tree_t *) first);
-	}
-	p = first->parent;
-	q = second->parent;
-	if (p == NULL || first == q || (!p->parent && q))
-	{
-		return (binary_trees_ancestor(first, q));
-	}
-	else if (q == NULL || p == second || (!q->parent && p))
-	{
-		return (binary_trees_ancestor(p, second));
-	}
-	return (binary_trees_ancestor(p, q));
+if (first == NULL || second == NULL)
+return (NULL);
+
+e = first;
+a = second;
+
+while (e != NULL)
+{
+while (a != NULL)
+{
+if (e == a)
+return ((binary_tree_t *)e);
+a = a->parent;
+}
+a = second;
+e = e->parent;
+}
+
+return (NULL);
 }
